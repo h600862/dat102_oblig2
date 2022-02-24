368adbd33c999ae0e300a7a86ee1a89f975aece2
h591099 <89256511+591099T@users.noreply.github.com>
2022-02-24 14:22:08 +0100

    Rekursjon

---
+++ Rekursjon/src/no/hvl/dat102/rekursjon/Main.java
@@ -0,0 +1,55 @@
+package no.hvl.dat102.rekursjon;
+
+import java.util.Scanner;
+
+public class Main {
+	private static RekursjonMetode metode;
+
+	public static void main(String[] args) {
+		
+//		int[] tab = { 11, 8, 4, 10, 2, 5, 7, 0, 61 };
+//		int maksVerdi = metode.maks(tab, 0, tab.length - 1);
+//		System.out.println(maksVerdi);
+//		
+		//********************************************************
+	
+		/**System.out.print("Oppgi et positivt heltall: ");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		System.out.println(n + "! = " + RekursjonMetode.fakultet(n));
+		System.out.println(n + "! = " + RekursjonMetode.fakultet(n));
+		System.out.println("Summen av " + n + " er " +   RekursjonMetode.sum(n));
+		System.out.println("Summen av " + n + " er " +   RekursjonMetode.sum2(n)); */
+		
+		
+		System.out.println("Oppgave 4 a)");
+		//  Oppgave 4 a) Sn = Sn-1 + n, S1 = 1
+		int resultat = RekursjonMetode.fakultet(100);
+		System.out.println(100 + "!=" +resultat);
+		System.out.println("Summen av n=100 er "+RekursjonMetode.sum(100));
+		
+		//  Oppgave 4 b) an = 5an-1 - 6an-2 + 2 for n > 1 og startkrav a0 = 2, a1 = 5
+		
+		System.out.println("Oppgave 4 b)");
+		int resultat2 = RekursjonMetode.fakultet(10);
+		System.out.println(10 + "!=" +resultat2);
+		System.out.println("Summen av n=100 er "+RekursjonMetode.sum(10));
+		
+		
+		//  Oppgave 4 c) fibrekursjon
+		System.out.println("Oppgave 4 c)");
+		int count=10;    
+		int n1=0,n2=1,n3=0;
+		System.out.print(n1+" "+n2);//printing 0 and 1    
+		RekursjonMetode.printFibonacci(count-2);//n-2 because 2 numbers are already printed
+		System.out.println();
+		//  Oppgave 4 d)  FibSec
+		System.out.println("Oppgave 4 d)");
+		
+		int sec = 10;
+		System.out.print(n1+" "+n2);//printing 0 and 1    
+		RekursjonMetode.fibSec(sec-2);//n-2 because 2 numbers are already printed
+		System.out.println();
+	}
+}
---
+++ Rekursjon/src/no/hvl/dat102/rekursjon/RekursjonMetode.java
@@ -0,0 +1,99 @@
+package no.hvl.dat102.rekursjon;
+
+public class RekursjonMetode{
+	
+	
+   /** public static int halv() {
+	if (maksVenstre >= maksHøgre){
+		return maksVenstre;
+		} else {
+		return maksHøgre;
+		}
+    }*/
+	
+	
+	public static int fakultet(int n) {
+		int svar;
+		if (n == 1) { // Basis
+		svar = 1;
+		}else {
+		svar = n * fakultet(n-1); // Rek. kall
+		}
+		return svar;
+		}
+	
+	public static int sum(int n){
+		//Summere de n første positive heltall
+		int svar;
+		if (n == 1){         // Basistilfelle
+		svar = 1; 
+		} 
+		else{
+		svar = n + sum(n-1); // Rek. kall
+		}
+		return svar;
+		}
+	
+	public static int sum2(int n) {
+		if (n == 1) { // Basis
+		return 1;
+		}else {
+		return n + sum2(n - 1);// Rek. kall
+		}
+		}
+	
+	
+	
+	static int maks(int[] tab, int start, int slutt) {
+		if (start == slutt) { // berre et element -> basis
+		return tab[start];
+		} else {
+		// Deler opp
+		int midten = (start + slutt) / 2;
+		// Rekursive kall
+		int mV = maks(tab, start, midten);
+		int mH = maks(tab, midten + 1, slutt);
+		// Setter sammen løsningene
+		if (mV >= mH) {
+		return mV;
+		} else {
+		return mH;
+		}
+		}
+}
+	static void fibSec(int n) {
+		int n1=0,n2=1,n3,i;    
+		// System.out.print(n1+" "+n2+" ");//printing 0 and 1    
+		    
+		 for(i=0;i<n;++i)//loop starts from 2 because 0 and 1 are already printed    
+		 {    
+		  n3=n1+n2;    
+		  System.out.print(" "+n3);    
+		  n1=n2;    
+		  n2=n3;    
+		  
+		 }    
+	}
+	static int n1=0,n2=1,n3=0;    
+	 static void printFibonacci(int n){    
+	    if(n>0){    
+	         n3 = n1 + n2;    
+	         n1 = n2;    
+	         n2 = n3;    
+	         System.out.print(" "+n3);   
+	         printFibonacci(n-1);    
+	     }    
+	 }
+	 
+	 static void tallfolge(int n){ 
+		 int a1=2,a2=5,a3=0;
+		    if(n>0){    
+		         a3 = a1 + a2;    
+		         a1 = a2;    
+		         a2 = a3;    
+		         System.out.print(" "+a3);   
+		         printFibonacci(n-1);    
+		     }    
+	 }
+}
+
